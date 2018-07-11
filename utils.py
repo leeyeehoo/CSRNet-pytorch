be0c3f4f017d42c23f76a725eeba0ee7c733d0a3
Yuhong Li <leeyh@minsky2-1.csl.illinois.edu>
2018-07-11 09:59:45 -0500

    2nd

---
+++ utils.py
@@ -0,0 +1,11 @@
+import h5py
+
+def save_net(fname, net):
+    with h5py.File(fname, 'w') as h5f:
+        for k, v in net.state_dict().items():
+            h5f.create_dataset(k, data=v.cpu().numpy())
+def load_net(fname, net):
+    with h5py.File(fname, 'r') as h5f:
+        for k, v in net.state_dict().items():        
+            param = torch.from_numpy(np.asarray(h5f[k]))         
+            v.copy_(param)
